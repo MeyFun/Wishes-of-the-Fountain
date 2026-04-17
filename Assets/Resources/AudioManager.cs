767bfe7520554f199326d109018efe766cfc64fe
MeyFun <marat.v.elena@mail.ru>
2026-04-17 22:38:33 +0600

    Исправил обновление звука при перезапуске
    При запуске музыка сразу играла громко, так как не обновлялась относительно настроек звука

--- Assets/Resources/AudioManager.cs
+++ Assets/Resources/AudioManager.cs
@@ -1,8 +1,11 @@
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class AudioManager : MonoBehaviour
 {
     public static AudioManager instance;
+
+    public AudioMixer mainMixer;
 
     [Header("Audio Sources")]
     public AudioSource musicSource;
@@ -31,6 +34,15 @@ public class AudioManager : MonoBehaviour
     public AudioClip launghBoySound;
     public AudioClip cryBoySound;
 
+
+    private void Start()
+    {
+        float savedMusicVolume = PlayerPrefs.GetFloat("MusicVolume", 0.75f);
+        float savedSFXVolume = PlayerPrefs.GetFloat("SFXVolume", 0.75f);
+
+        mainMixer.SetFloat("MusicVol", Mathf.Log10(savedMusicVolume) * 20);
+        mainMixer.SetFloat("SFXVol", Mathf.Log10(savedSFXVolume) * 20);
+    }
 
     void Awake()
     {
